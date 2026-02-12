42cd6396bcb151dd9ec72d887d995531c5318b4c
Timur Siraev <siraev.tima91@gmail.com>
2026-02-12 15:13:43 +0400

    feat: implement Art Space (lab-2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "com.example.artspace"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(36)
     }
 
     defaultConfig {
